d5cfe3655343b43960e508ffabe6de4adbf97356
Local <thetypgame@gmail.com>
2023-04-14 22:05:09 +0400

    :ambulance: fix: tsconfig.json change importHelpers flag to false

--- dist/simulator/Simulator.js
+++ dist/simulator/Simulator.js
@@ -1,4 +1,12 @@
-import { __spreadArray } from "tslib";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 import React, { useEffect, useRef, useState } from 'react';
 import { Commands } from './commands';
 import styles from './Simulator.module.css';
