d0322f8687360e5f6d465b3cbb6ee6af48c07bf1
Local <thetypgame@gmail.com>
2023-04-18 14:22:00 +0400

    :zap: perf: .idea & minify
    Removed unnecessary files and minimized the build folder

--- dist/lines/Output.js
+++ dist/lines/Output.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import LineStart from './LineStart';
-import styles from '../simulator/Simulator.module.css';
-var Output = function (_a) {
-    var user = _a.user, name = _a.name, path = _a.path, command = _a.command, prompt = _a.prompt, children = _a.children, _b = _a.lineStart, lineStart = _b === void 0 ? true : _b;
-    return (React.createElement(React.Fragment, null,
-        lineStart && (React.createElement(LineStart, { user: user, name: name, path: path, prompt: prompt }, command)),
-        React.createElement("span", { className: styles.simulator__line }, children)));
-};
-export default React.memo(Output);
-//# sourceMappingURL=Output.js.map
+import React from"react";import LineStart from"./LineStart";import styles from"../simulator/Simulator.module.css";var Output=function(_a){var user=_a.user,name=_a.name,path=_a.path,command=_a.command,prompt=_a.prompt,children=_a.children,_b=_a.lineStart,lineStart=_b===void 0?true:_b;return React.createElement(React.Fragment,null,lineStart&&React.createElement(LineStart,{user:user,name:name,path:path,prompt:prompt},command),React.createElement("span",{className:styles.simulator__line},children))};export default React.memo(Output);
--- dist/simulator/commands.js
+++ dist/simulator/commands.js
@@ -1,15 +1,1 @@
-export var Commands;
-(function (Commands) {
-    Commands["help"] = "help";
-    Commands["ls"] = "ls";
-    Commands["cd"] = "cd";
-    Commands["cat"] = "cat";
-    Commands["whoami"] = "whoami";
-    Commands["clear"] = "clear";
-    Commands["mkdir"] = "mkdir";
-    Commands["echo"] = "echo";
-    Commands["rm"] = "rm";
-    Commands["cp"] = "cp";
-    Commands["rev"] = "rev";
-})(Commands || (Commands = {}));
-//# sourceMappingURL=commands.js.map
+export var Commands;(function(Commands){Commands["help"]="help";Commands["ls"]="ls";Commands["cd"]="cd";Commands["cat"]="cat";Commands["whoami"]="whoami";Commands["clear"]="clear";Commands["mkdir"]="mkdir";Commands["echo"]="echo";Commands["rm"]="rm";Commands["cp"]="cp";Commands["rev"]="rev"})(Commands||(Commands={}));
